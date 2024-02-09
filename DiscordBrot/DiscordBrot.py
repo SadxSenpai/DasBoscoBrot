fb1f46d45e7c3cbdc503069faf412a06daf243e7
SadxSenpai <Marvinhauser14@gmail.com>
2024-02-09 16:43:57 +0100

    counter test try 1

--- DiscordBrot/DiscordBrot.py
+++ DiscordBrot/DiscordBrot.py
@@ -184,13 +184,16 @@ async def pdf_loop():
     popplerpath = r'poppler-23.11.0\Library\bin'
     
     counter2 = 1
+    counter4 = 1   
+
     filenames = sorted(os.listdir('Speiseplan'), reverse=False)
     for filename in os.listdir('Speiseplan'):
         if filename.endswith('.pdf'):
-            pages = convert_from_path(f'Speiseplan/{filename}', 500, poppler_path=popplerpath, first_page=1, last_page=1)
+            pages = convert_from_path(f'Speiseplan/file' + str(counter4) + '.pdf', 500, poppler_path=popplerpath, first_page=1, last_page=1)
             for page in pages:
                 page.save('Speiseplan/essen' + str(counter2) +'.jpg', 'JPEG')
-                counter2 += 1        
+                counter2 += 1
+                counter4 += 1
                 print(f"> {Style.BRIGHT}{filename}{Style.RESET_ALL} converted to image")
                 time.sleep(2)
                 
@@ -205,7 +208,7 @@ async def pdf_loop():
     channel = client.get_channel(1166651023822159882)
     
     #send all .jpg files in the folder Speiseplan in reversed alphabetical order
-    counter3 = 1
+    counter3 = 1 
     
     # Get the list of files, sort them in reverse alphabetical order
     filenames = sorted(os.listdir('Speiseplan'), reverse=True)
